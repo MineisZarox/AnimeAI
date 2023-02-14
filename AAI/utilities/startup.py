f011c1b48f634d07370ff4463b2bb8fd50a90a13
Zarox <85471432+MineisZarox@users.noreply.github.com>
2023-02-15 01:06:44 +0530

    Update startup.py

--- AAI/utilities/startup.py
+++ AAI/utilities/startup.py
@@ -2,4 +2,4 @@ from .. import aai, Vars
 from telethon import Button
 
 async def startup():
-    await aai.send_message(Vars.LOG_GRP, "**AnimeAI has been started successfully.**", buttons=[(Button.url("Shinichi", "https://t.me/catuserbotot"),)],)
+    await aai.send_message(Vars.LOG_GRP, "**AnimeAI has been started from host successfully.**", buttons=[(Button.url("Shinichi", "https://t.me/catuserbotot"),)],)
